50a5831dddb534c6a57e54fae28654c32c7a773f
Hoàng Đức Quang <hdquang6@gmail.com>
2026-05-19 15:00:17 +0700

    Initial commit

---
+++ PhoneShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PhoneShop.DB;
+using PhoneShop.Models;
+
+namespace PhoneShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private PhoneShopDbContext db;
+
+        public ProductController(PhoneShopDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var prods = await db
+                .Products
+                .Include(p=>p.Category)
+                .ToListAsync();
+            return View(prods);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var cates = await db.Categories
+                .Select(c => new SelectListItem(
+                    c.Name, c.Id.ToString())).ToListAsync();
+            ViewBag.CategoryId = cates;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(p);
+            }
+
+            await db.Products.AddAsync(p);
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
